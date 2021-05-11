fe89d1e5c69780fa96fc9a28ba41be1a28098275
zjardyn <zjardyn@gmail.com>
2021-05-11 15:34:12 -0400

    chapter 1 third edit

--- Chapter1.R
+++ Chapter1.R
@@ -142,4 +142,111 @@ ggplot(data = mpg) +
 ggplot(data = mpg) +
   geom_point(mapping = aes(x=displ, y=hwy, color=drv))
 
+# ----
+# different geoms
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x=displ, y=hwy))
+# try other categorical vars
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x=displ, y=hwy, linetype=drv))
+
+# every geom function takes a mapping arg but not every aesthetic works with every geom
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x=displ, y=hwy))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x=displ, y=hwy, group =drv))
+
+ggplot(data = mpg) +
+  geom_smooth(mapping = aes(x=displ, y=hwy, color =drv),
+              show.legend = F)
+
+# add multiple geom layers
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x=displ, y=hwy)) +
+  geom_smooth(mapping = aes(x=displ, y=hwy))
+
+# less redundant
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy) ) +
+  geom_point() +
+  geom_smooth()
+
+# different aesthetics in different layers
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy) ) +
+  geom_point(mapping = aes(colour= class)) +
+  geom_smooth() # can add a group mapping here too!
+
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy) ) +
+  geom_point(mapping = aes(colour= class)) +
+  geom_smooth(
+    data=filter(mpg, class == "subcompact"),
+    se = F)
+
+# Exercises 4 ----
+# geome_smooth for line charts
+
+# notice how color=drv will affect the lines!
+ggplot(
+  data = mpg,
+  mapping = aes(x=displ, y=hwy,color=drv) 
+  ) +
+  geom_point() +
+  geom_smooth(se=FALSE) 
+
+# show.legend = F removes the legend 
+
+?geom_smooth
+
+# se=F removes confidence interval from loess line!
+
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy)) +
+  geom_point() +
+  geom_smooth()
+
+# or 
+
+ggplot() +
+  geom_point(data = mpg,
+             mapping = aes(x=displ, y=hwy)
+             ) +
+  geom_smooth(data = mpg,
+              mapping = aes(x=displ, y=hwy)
+              )
+
+# 6 graphs 
+
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy)) +
+  geom_point() +
+  geom_smooth(se=F)
+
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy, group = drv)) +
+  geom_point() +
+  geom_smooth(se=F)
+
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy, color = drv)) +
+  geom_point() +
+  geom_smooth(se=F)
+
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy)) +
+  geom_point(mapping = aes(color = drv)) +
+  geom_smooth(se=F)
+
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy)) +
+  geom_point(mapping = aes(color = drv)) +
+  geom_smooth(se=F,mapping = aes(linetype = drv))
+
+
+ggplot(data = mpg, mapping = aes(x=displ, y=hwy)) +
+  geom_point(mapping = aes(color = drv)) 
+
+# statistical transformations
+
+
+
+
+
+
 
