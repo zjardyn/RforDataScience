5517ef106c4331fbc9d8c0341aa1ab6f0146cd10
zjardyn <zjardyn@gmail.com>
2021-05-12 00:48:51 -0400

    Chapter 2

---
+++ Chapter2.R
@@ -0,0 +1,23 @@
+1/100 *30
+(59 + 73 + 2) / 3
+sin(pi / 2)
+
+# alt - is assignment shortcut
+x <- 3 
+x
+
+this_is_a_really_long_name <- 2.5
+
+r_rocks <- 2 ^ 3
+
+r_rock
+R_rocks
+
+# function
+seq(1, 10)
+x <- "Hello"
+# hit escape to exit
+y <- seq(1, 10, length.out = 5)
+y
+# equivalent
+(y <- seq(1, 10, length.out = 5))
