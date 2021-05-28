d62ef43eb8fb9fdb1803dddc5745c5bfcc555718
zjardyn <lierahoz@uoguelph.ca>
2021-05-28 17:43:21 -0400

    chapter 7 done

---
+++ Chapter7.R
@@ -0,0 +1,14 @@
+# Wrangling 
+# In tibbles, you’ll learn about the variant of the data frame that we use in this book: the tibble. You’ll learn what makes them different from regular data frames, and how you can construct them “by hand”.
+
+# In data import, you’ll learn how to get your data from disk and into R. We’ll focus on plain-text rectangular formats, but will give you pointers to packages that help with other types of data.
+
+# In tidy data, you’ll learn about tidy data, a consistent way of storing your data that makes transformation, visualisation, and modelling easier. You’ll learn the underlying principles, and how to get your data into a tidy form.
+
+# Relational data will give you tools for working with multiple interrelated datasets.
+
+# Strings will introduce regular expressions, a powerful tool for manipulating strings.
+
+# Factors are how R stores categorical data. They are used when a variable has a fixed set of possible values, or when you want to use a non-alphabetical ordering of a string.
+
+# Dates and times will give you the key tools for working with dates and date-times.
